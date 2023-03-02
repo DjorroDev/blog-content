b1d6b36677d3bb37400897a4bf65fff001a299c0
Djorro <DjorroDev@users.noreply.github.com>
2023-03-02 20:16:17 +0700

    Update prevNext to support mobile

--- components/PrevNext.vue
+++ components/PrevNext.vue
@@ -21,7 +21,7 @@ defineProps(['prev', 'next'])
   
   <style scoped>
   .prev-next-cont {
-    @apply flex gap-4 justify-between p-4 border border-slate-200 rounded-lg;
+    @apply flex flex-col md:flex-row gap-4 justify-between p-4 border border-slate-200 rounded-lg;
   }
   .link-item a {
     @apply flex gap-2;
