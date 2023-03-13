537d7849e098ec2cde8e12520efb2995b6fe09fb
Djorro <DjorroDev@users.noreply.github.com>
2023-03-13 11:45:16 +0700

    Update page style

--- pages/blog/tags/[slug].vue
+++ pages/blog/tags/[slug].vue
@@ -51,4 +51,29 @@ useHead({
 .cta.w-icon {
   @apply flex items-center gap-2 pr-3;
 }
+
+.page-heading {
+  @apply p-12 bg-slate-50;
+}
+
+.page-heading>.wrapper {
+  @apply max-w-3xl m-auto;
+}
+
+.page-section {
+  @apply p-4 py-8 m-auto max-w-3xl;
+}
+
+.icon.stroke {
+  @apply stroke-slate-500;
+}
+
+.cta {
+  @apply p-1.5 px-2 bg-slate-50 border border-slate-200 font-semibold rounded-lg;
+  @apply transition-all hover:-translate-y-0.5 focus:bg-slate-100 active:translate-y-0.5 active:shadow-inner;
+}
+
+.cta.w-icon {
+  @apply flex items-center gap-2 pr-3;
+}
 </style>
